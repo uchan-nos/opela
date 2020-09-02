c578dac4f155b70bfb6c0596aa984bfe265663e5
uchan-nos <uchan0@gmail.com>
2020-09-02 10:51:14 +0900

    refactor test script

--- compiler/test.sh
+++ compiler/test.sh
@@ -2,21 +2,35 @@
 
 clang++-10 -O3 -std=c++20 -o opelac main.cpp
 
+passed=0
+failed=0
+
 function build_run() {
-  echo $2 | ./opelac > tmp.s
+  want="$1"
+  input="$2"
+
+  echo "$input" | ./opelac > tmp.s
   nasm -f elf64 -o tmp.o tmp.s
   cc -o tmp tmp.o
   ./tmp
-  ANSWER=$?
+  got=$?
 
-  if [ $1 = $ANSWER ]
+  if [ "$want" = "$got" ]
   then
-    echo "[  OK  ]: got $ANSWER, input $2"
+    echo "[  OK  ]: $input -> $got"
+    (( ++passed ))
   else
-    echo "[FAILED]: want $1, got $ANSWER, input $2"
+    echo "[FAILED]: $input -> $got, want $want"
+    (( ++failed ))
   fi
 
   rm tmp tmp.o tmp.s
 }
 
 build_run 42 42
+
+echo "$passed passed, $failed failed"
+if [ $failed -ne 0 ]
+then
+  exit 1
+fi
