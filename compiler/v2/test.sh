e98f31d9c01e88fb078c35d69555c1d053cf8692
uchan-nos <uchan@uchan.net>
2021-09-02 12:00:02 +0900

    テスト実行を test.opl 方式に戻す

--- compiler/v2/test.sh
+++ compiler/v2/test.sh
@@ -73,45 +73,45 @@ function test_argv() {
   fi
 }
 
-#make test.exe || exit 1
+make test.exe || exit 1
 
 echo "Running standard testcases..."
-#./test.exe
-test_exit 42 'func main() int { return 42; }'
-test_exit 30 'func main() int { return (1+2) / 2+ (( 3 -4) +5 *  6 ); }'
-test_exit 5  'func main() int { return -3 + (+8); }'
-test_exit 2  'func main() int { return -2 * -1; }'
-test_exit 0  'func main() int { return 3 < (1 + 1); }'
-test_exit 1  'func main() int { return 3 > (1 + 1); }'
-test_exit 1  'func main() int { return 2*3 >= 13/2; }'
-test_exit 1  'func main() int { return 2>2 == 4<=3; }'
-test_exit 15 'func main() int { foo:=5; bar:=3; return foo*bar; }'
-test_exit 6  'func main() int { foo:=2; { foo:=3; } return foo+4; }'
-test_exit 42 'func main() int { return 41+1; 3; }'
-test_exit 2  'func main() int { if 42 > 10 { return 2; } }'
-test_exit 0  'func main() int { if 42 < 10 { return 2; } }'
-test_exit 2  'func main() int { cond := 10 < 200; if cond { return 2; } }'
-test_exit 4  'func main() int { if 0 { return 3; } else { return 4; } }'
-test_exit 5  'func main() int { if 0 { return 3; } else if 1 { return 5; } else { return 4; } }'
-test_exit 8  'func main() int { foo := 3; foo = 4; return foo * 2; }'
-test_exit 38 'func main() int { foo:=5; bar:=7; foo=(bar=1)=42; return foo-4; }'
-test_exit 42 'func main() int { foo:=5; bar:=7; return foo=bar=42; }'
-test_exit 9  'func main() int { a:=5; a=b:=3; return a*b; }'
-test_exit 55 'func main() int { i:=0; s:=0; for i <= 10 { s=s+i; i=i+1; } return s; }'
-test_exit 55 'func main() int { s:=0; for i:=0; i<=10; i=i+1 { s=s+i; } return s; }'
-test_exit 9  'func main() int { s:=0; for i:=1;i<3;i=i+1{ for j:=1;j<3;j=j+1{ s=s+i*j; } } return s; }'
-test_exit 39 'func main() int { return func42() - 3; } extern "C" func42 func()int;'
-test_exit 42 'func main() int { return funcfunc42()(); } extern "C" funcfunc42 func() *func()int;'
-test_exit 43 'func main() int { return add((1+2)*(3+4), add(func42(), 1)) - 21; } extern "C" func42 func()int; extern "C" add func(a, b int) int;'
-test_exit 4  'func main() int { return sizeof(myInt); } type myInt int32;'
-test_exit 6  'func main() int { return (3@myInt2 + 2@int2) + 5; } type myInt2 int2;'
-test_exit 57 'func main() int {return add(sum10(), 2);} extern "C" add func(a,b int)int; func sum10()int{s:=0; for i:=1;i<=10;i=i+1 {s=s+i;} return s;}'
-test_exit 5  'func main() int {return myAdd(-3,8);} func myAdd(a,b int)int{return a+b;}'
+./test.exe
+#test_exit 42 'func main() int { return 42; }'
+#test_exit 30 'func main() int { return (1+2) / 2+ (( 3 -4) +5 *  6 ); }'
+#test_exit 5  'func main() int { return -3 + (+8); }'
+#test_exit 2  'func main() int { return -2 * -1; }'
+#test_exit 0  'func main() int { return 3 < (1 + 1); }'
+#test_exit 1  'func main() int { return 3 > (1 + 1); }'
+#test_exit 1  'func main() int { return 2*3 >= 13/2; }'
+#test_exit 1  'func main() int { return 2>2 == 4<=3; }'
+#test_exit 15 'func main() int { foo:=5; bar:=3; return foo*bar; }'
+#test_exit 6  'func main() int { foo:=2; { foo:=3; } return foo+4; }'
+#test_exit 42 'func main() int { return 41+1; 3; }'
+#test_exit 2  'func main() int { if 42 > 10 { return 2; } }'
+#test_exit 0  'func main() int { if 42 < 10 { return 2; } }'
+#test_exit 2  'func main() int { cond := 10 < 200; if cond { return 2; } }'
+#test_exit 4  'func main() int { if 0 { return 3; } else { return 4; } }'
+#test_exit 5  'func main() int { if 0 { return 3; } else if 1 { return 5; } else { return 4; } }'
+#test_exit 8  'func main() int { foo := 3; foo = 4; return foo * 2; }'
+#test_exit 38 'func main() int { foo:=5; bar:=7; foo=(bar=1)=42; return foo-4; }'
+#test_exit 42 'func main() int { foo:=5; bar:=7; return foo=bar=42; }'
+#test_exit 9  'func main() int { a:=5; a=b:=3; return a*b; }'
+#test_exit 55 'func main() int { i:=0; s:=0; for i <= 10 { s=s+i; i=i+1; } return s; }'
+#test_exit 55 'func main() int { s:=0; for i:=0; i<=10; i=i+1 { s=s+i; } return s; }'
+#test_exit 9  'func main() int { s:=0; for i:=1;i<3;i=i+1{ for j:=1;j<3;j=j+1{ s=s+i*j; } } return s; }'
+#test_exit 39 'func main() int { return func42() - 3; } extern "C" func42 func()int;'
+#test_exit 42 'func main() int { return funcfunc42()(); } extern "C" funcfunc42 func() *func()int;'
+#test_exit 43 'func main() int { return add((1+2)*(3+4), add(func42(), 1)) - 21; } extern "C" func42 func()int; extern "C" add func(a, b int) int;'
+#test_exit 4  'func main() int { return sizeof(myInt); } type myInt int32;'
+#test_exit 6  'func main() int { return (3@myInt2 + 2@int2) + 5; } type myInt2 int2;'
+#test_exit 57 'func main() int {return add(sum10(), 2);} extern "C" add func(a,b int)int; func sum10()int{s:=0; for i:=1;i<=10;i=i+1 {s=s+i;} return s;}'
+#test_exit 5  'func main() int {return myAdd(-3,8);} func myAdd(a,b int)int{return a+b;}'
 
 echo "============================="
-#echo "Running extra testcases..."
-#test_stdout 'foo' 'func main() { write(1, "foo", 3); }
-#  extern "C" write func(int, *byte, int);'
+echo "Running extra testcases..."
+test_stdout 'foo' 'func main() { write(1, "foo", 3); }
+  extern "C" write func(int, *byte, int);'
 
 echo "$passed passed, $failed failed"
 if [ $failed -ne 0 ]
